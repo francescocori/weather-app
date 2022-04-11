76cad79f4d996dac2669ab562801858d4baff187
franncesco <francesco.coriolano@gmail.com>
2022-04-11 15:32:29 +0200

    set up structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import HomePage from "./Components/Homepage";
+import WeatherPage from "./Components/WeatherPage";
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="App">
+      hello world
+      <HomePage />
+      <WeatherPage />
+    </div>
+  );
 }
 
 export default App;
