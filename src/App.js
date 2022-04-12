8aaa92e02dbecdb815947e09c57d8eeb061c748d
franncesco <francesco.coriolano@gmail.com>
2022-04-12 10:48:24 +0200

    showing icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 import HomePage from "./Components/Homepage";
 import WeatherPage from "./Components/WeatherPage";
 
 function App() {
   const apiKey = "edc98e7954f86bbfa9e8ef821e348c92";
   const [city, setCity] = useState("");
-  const [data, setData] = useState([]);
-  const getWeather = (e) => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data);
-        console.log(data);
-      });
+  const [data, setData] = useState();
+
+  function getWeather() {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`
+      )
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch((err) => console.log(err));
+  }
+  const handlClick = () => {
+    getWeather();
+    setCity("");
   };
-  console.log(city);
+  // const getWeather = (e) => {
+  //   fetch(
+  //     `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`
+  //   )
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setData(data);
+  //       console.log(data);
+  //       console.log(data.main.temp);
+  //     });
+  // };
 
   //axios get data from Api
 
@@ -32,9 +49,14 @@ function App() {
           value={city}
         ></input>
       </form>
-      <button type="submit" onClick={getWeather}>
+      <button type="submit" onClick={handlClick}>
         search..
       </button>
+      {data && <h1>{data.main.temp}</h1>}
+      {data && <h1>{data.weather[0].main}</h1>}
+      <img
+        src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+      />
     </div>
   );
 }
