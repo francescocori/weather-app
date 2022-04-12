34c84a2477bb7ad1c8e38408981b6a43408f3515
franncesco <francesco.coriolano@gmail.com>
2022-04-12 09:43:41 +0200

    getting data from api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
+import { useState } from "react";
 import "./App.css";
 import HomePage from "./Components/Homepage";
 import WeatherPage from "./Components/WeatherPage";
 
 function App() {
+  const apiKey = "edc98e7954f86bbfa9e8ef821e348c92";
+  const [city, setCity] = useState("");
+  const [data, setData] = useState([]);
+  const getWeather = (e) => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  };
+  console.log(city);
+
+  //axios get data from Api
+
   return (
     <div className="App">
       hello world
-      <HomePage />
-      <WeatherPage />
+      <form>
+        <input
+          type="text"
+          name="cit"
+          onChange={(e) => setCity(e.target.value)}
+          value={city}
+        ></input>
+      </form>
+      <button type="submit" onClick={getWeather}>
+        search..
+      </button>
     </div>
   );
 }
