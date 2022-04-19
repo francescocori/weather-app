5631fd39d0411501ab2e1793af2334fc513a922a
franncesco <francesco.coriolano@gmail.com>
2022-04-19 08:53:59 +0200

    rebuild autocoplite searchbar working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import data from "./Data/city2.list.json";
 import axios from "axios";
-import WeatherPage from "./Components/WeatherPage";
-import SearchBar from "./Components/SearchBar";
-import SearchBarN from "./Components/SeatchBarN";
-
-function App() {
-  const apiKey = process.env.REACT_APP_API_KEY;
-  const [city, setCity] = useState(""); //text inside the input
-  const [data, setData] = useState(); // all city list
-  const [choosed, setChoosed] = useState();
+const App = () => {
   const [input, setInput] = useState("");
-  const [coord, setCoord] = useState({
-    lon: 0,
-    lan: 0,
-  });
-  const [dataFound, setDataFound] = useState([]);
+  const [matchesArray, setMatchesArray] = useState([]);
+  const [city, setCity] = useState("");
+  const [locationData, setLocationData] = useState();
+  const apiKey = process.env.REACT_APP_API_KEY;
 
-  //get data from API
+  //get weather info
   const getWeather = () => {
     axios
       .get(
@@ -25,164 +16,91 @@ function App() {
       )
       .then((res) => {
         console.log(res.data);
-        setData(res.data);
+        let respo = res.data;
+        setLocationData(respo);
       })
       .catch((err) => console.log(err));
   };
 
-  //handle Click / Submit
-  const handlSubmit = (e) => {
-    console.log("city is", city);
-    // setCity(city);
-    getWeather();
-    // setCity("");
-  };
-  // create date of today
-  const dateBuilder = (d) => {
-    let months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
-
-    return `${day} ${date} ${month} ${year}`;
-  };
-
+  //filter suggestions
   const getCitySuggestions = (input) => {
-    let resp = [];
+    let matches = [];
     if (input !== "") {
-      for (let i = 0; i < data.length && resp.length < 10; i++) {
+      for (let i = 0; i < data.length && matches.length < 10; i++) {
         if (data[i].name.toLowerCase().startsWith(input.toLowerCase())) {
-          resp.push({
+          matches.push({
             id: data[i].id,
             long: data[i].coord.lon,
             lat: data[i].coord.lat,
             name: data[i].name,
             country: data[i].country,
           });
-          setDataFound(resp);
-          console.log("yesss", resp);
+          setMatchesArray(matches);
         }
       }
     }
-    return resp;
+    return matches;
   };
+
+  //call function every time user type
   useEffect(() => {
     getCitySuggestions(input);
-    // setDataFound(result);
   }, [input]);
 
-  return (
-    <div className="App">
-      <SearchBarN handlSubmit={handlSubmit} city={city} />
-      <SearchBar
-        getWeather={getWeather}
-        dataFound={dataFound}
-        setDataFound={setDataFound}
-        setCity={setCity}
-        handlSubmit={handlSubmit}
-        input={input}
-        setInput={setInput}
-        city={city}
-      />
-      {/* +++++++++++++ ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/}
+  // handle when user click on city
+  const handleClick = (cityName) => {
+    setCity((prev) => cityName);
+    setInput((prev) => cityName);
+  };
 
-      <form onSubmit={handlSubmit}>
+  //handle Click / Submit
+  const handlSubmit = (e) => {
+    if (city !== "") {
+      getWeather();
+    }
+    console.log("city is", city);
+  };
+
+  return (
+    <div>
+      <form>
         <input
           type="text"
-          name="cit"
-          onChange={(e) => setCity(e.target.value)}
-          value={city}
-        ></input>
-        <button type="submit">search..</button>
+          name="search"
+          id="search"
+          placeholder="search..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="button" onClick={handlSubmit}>
+          search..
+        </button>
+        {matchesArray.map((item, index) => (
+          <h4
+            onClick={() => handleClick(item.name)}
+            value={item.name}
+            key={item.id}
+          >
+            {item.name}
+          </h4>
+        ))}
       </form>
-      <br />
-      <h1>{dateBuilder(new Date())}</h1>
 
-      {data && (
+      {locationData && (
         <>
-          <h1>{data.name}</h1>
-          <h2>{Math.round(data.main.temp)}°C</h2>{" "}
-          <h2>{data.weather[0].main}</h2>
+          <h1>{locationData.name}</h1>
+          <h2>{Math.round(locationData.main.temp)}°C</h2>{" "}
+          <h2>{locationData.weather[0].description}</h2>
           <img
-            src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+            src={`http://openweathermap.org/img/wn/${locationData.weather[0].icon}@2x.png`}
             alt="icon"
           />
-          <h3>Lon:{data.coord.lon}</h3>
-          <h3>Lat:{data.coord.lat}</h3>
+          <h3>Lon:{locationData.coord.lon}</h3>
+          <h3>Lat:{locationData.coord.lat}</h3>
         </>
       )}
     </div>
   );
-}
+};
 
 export default App;
-
-// return (
-//   <div className="App">
-
-// {/* <SearchBar
-// dataFound={dataFound}
-// setDataFound={setDataFound}
-// setCity={setCity}
-// handlClick={handlClick}
-// input={input}
-// setInput={setInput}
-// city={city}
-// /> */}
-//     {/* +++++++++++++ ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/}
-
-//      <form onSubmit={handlClick}>
-//       <input
-//         type="text"
-//         name="cit"
-//         onChange={(e) => setCity(e.target.value)}
-//         value={city}
-//         ///not working
-//       ></input>
-//       <button type="submit">search..</button>
-//     </form>
-//     <br />
-//     <h1>{dateBuilder(new Date())}</h1>
-
-//     {data && (
-//       <>
-//         <h1>{data.name}</h1>
-//         <h2>{Math.round(data.main.temp)}°C</h2>{" "}
-//         <h2>{data.weather[0].main}</h2>
-//         <img
-//           src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
-//           alt="icon"
-//         />
-//         <h3>Lon:{coord.lon}</h3>
-//         <h3>Lat:{data.coord.lat}</h3>
-//       </>
-//     )}
-//   </div>
-// );
-// }
-
-// export default App;
